04d7846b3539bd6571df976a307580a4e1c4d627
straightupgabe <90867229+straightupgabe@users.noreply.github.com>
2021-09-27 13:19:34 -0600

    Update ImportModule.py

--- PhoneBook/ImportModule.py
+++ PhoneBook/ImportModule.py
@@ -1,5 +1,7 @@
 print()
 
+#assigns variables to made up phonenumbers to be used in mainprogram.py
+#also assigns phone numbers as integer values instead of string values (as a precautionary measure)
 Towing_Company = int(7802456777)
 Laundry_Company = int(4406021222)
 Computer_Company = int(9091112765)
